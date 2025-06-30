458cc9ccc0972d46944b7d213445d03a73609db7
piyushsankhala <piyushsankhala2004@gmail.com>
2025-06-30 19:07:15 +0530

    Update otp.routes.js

--- src/routes/otp.routes.js
+++ src/routes/otp.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { sendotp } from '../controllers/otp.controllers.js';
-import { verifyotp } from '../controllers/otp.controllers.js';
+import { sendotp } from '../controllers/otp.controller.js';
+import { verifyotp } from '../controllers/otp.controller.js';
 
 const router = express.Router();
 router.post('/send', sendotp);
